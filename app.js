572f6ed6bf87fc64d4f035ef4a7bcabfaef78e7f
elias-kodehode <elias96.kodehode@gmail.com>
2026-03-10 10:11:10 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const app = {};
-
-var mouseElement = document.getElementById("mouse");
-
+app.mouseElement = document.getElementById("mouse");
 app.expand = (obj) =>  {
     var parent = obj.parentElement.parentElement;
     
     var isExpanded = parent.classList.contains("expand");
 
-    if(!isExpanded){
-        parent.classList.add("expand");
-    }else{
+    isExpanded == false ? 
+        parent.classList.add("expand") :
         parent.classList.remove("expand");
-    }
 };
 
 document.addEventListener("mousemove", function(evt)  {
-    const x = evt.clientX;
-    const y = evt.clientY;
-
-    mouseElement.style.left = x + "px";
-    mouseElement.style.top = y + "px";
-})
+    app.mouseElement.style.left = evt.clientX + "px";
+    app.mouseElement.style.top = evt.clientY + "px";
+});
